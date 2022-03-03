5ad987649de45cd1b2bba075d9de542f26977127
Aniket9604 <100848009+Aniket9604@users.noreply.github.com>
2022-03-03 19:09:05 +0530

    Update Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -7,12 +7,6 @@ public class Demo {
 <<<<<<< HEAD
 		System.out.println("hello aniket");
 =======
-		System.out.println("hello aniiket");
-		System.out.prinltn(" thanks nisha" );
-		System.out.println("great you are!!" );
->>>>>>> branch 'master' of https://github.com/Aniket9604/Selenium.git
-	System.out.println(" hi friends ");
-
 	}
 
 }
